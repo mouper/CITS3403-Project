a3849ccf8ed0d5c5ef62e4add54bd7f543ebc07e
mouper <51354655+mouper@users.noreply.github.com>
2025-04-17 21:25:41 +0800

    Update app.py to use routes.py
    Improves functionality + usage of Flask

--- src/app.py
+++ src/app.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    @app.route('/index')
-    def index():
-        return render_template(
-            'index.html', title='home'
-        )
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+application = Flask(__name__)
+import routes
